ca6d89c3103a32823fa05cb8e357f7921d6b73c2
Ertuğrul Kuran <ertkrn@hotmail.com>
2018-05-28 02:32:31 +0300

    SSS

---
+++ ForumErtkrn/obj/Release/Package/PackageTmp/Scripts/menu-degistir.js
@@ -0,0 +1,9 @@
+﻿$('#footer a').click(function () {
+    var b = document.getElementById("menuid").innerHTML;
+    var a = $('#menuid');
+    a.remove();
+    $('#' + b + ' br').remove();
+    var c = $(this).attr('id');
+    var ekle = "<br /><strong id='menuid'>" + c + "</strong>";
+    $(this).append(ekle);
+});
