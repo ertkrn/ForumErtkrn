3c619558dc9b13176d3f81c952d28ac5e643d781
Ertuğrul Kuran <ertkrn@hotmail.com>
2018-05-28 05:32:30 +0300

    SSS

---
+++ ForumErtkrn/obj/Release/Package/PackageTmp/Scripts/menu-gizle.js
@@ -0,0 +1,13 @@
+﻿setTimeout(function () {
+    $('#footer').hide();
+    $('#scrlltp').hide();
+}, 5000);
+
+window.addEventListener('touchstart', function () {
+    $('#footer').removeAttr('style');
+    $('#scrlltp').removeAttr('style');
+    setTimeout(function () {
+        $('#footer').hide();
+        $('#scrlltp').hide();
+    }, 5000);
+});
